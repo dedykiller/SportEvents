7617eecee99a7e85acfbe71978fc2f58cad70f14
dedykiller <dedykiller@seznam.cz>
2014-11-04 15:43:40 +0100

    init commit

---
+++ SportsEvents/SportsEvents/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SportsEvents.MvcApplication" Language="C#" %>
---
+++ SportsEvents/SportsEvents/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportsEvents.Startup))]
+namespace SportsEvents
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
