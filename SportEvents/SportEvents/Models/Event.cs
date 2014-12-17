a5411f56c44a812379a9c470feb47595ee693e2c
Jan Dedek <dedykiller@seznam.cz>
2014-12-18 00:57:45 +0100

    commint z nutnosti

---
+++ SportEvents/SportEvents/Models/Event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportEvents.Models
+{
+    public enum TypeOfPayment 
+    {
+        Cash,
+        AfterPeriod
+
+    }
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime TimeOfEvent { get; set; }
+        public string Place { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public  TypeOfPayment Payment { get; set;}
+        public virtual ICollection<Group> Groups { get; set; }
+
+        public Event()
+        {
+            Groups = new HashSet<Group>();
+        }
+    }
+
+    
+}
