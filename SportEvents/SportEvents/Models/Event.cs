8b01689a4af0c07f732510b4964218d5e4358f22
Jan Dedek <dedykiller@seznam.cz>
2014-12-18 01:06:43 +0100

    migrace

--- SportEvents/SportEvents/Models/Event.cs
+++ SportEvents/SportEvents/Models/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -14,10 +15,15 @@ namespace SportEvents.Models
     public class Event
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Vyplňte prosím název události")]
         public string Name { get; set; }
         public DateTime TimeOfEvent { get; set; }
         public string Place { get; set; }
         public string Description { get; set; }
+
+        [RegularExpression("^[0-9][0-9]*$",
+            ErrorMessage = "Cena události musí být kladné číslo.")]
+
         public int Price { get; set; }
         public  TypeOfPayment Payment { get; set;}
         public virtual ICollection<Group> Groups { get; set; }
